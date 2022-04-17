bf3fe84b44b679f15df64e7d4f11f7af2be65915
scott-diprose <scott.on.github@gmail.com>
2022-04-17 23:20:29 +1000

    Thoughs on sub packages

---
+++ src/pydmm/utils/upgrade/mapping.py
@@ -0,0 +1,5 @@
+
+# Due to dependencies, maybe this is probably best not to be here? Perhaps a cmdline util.
+
+# from prev_ver import YamlLoader as old_loader
+from ...mapping import YamlLoader as new_loader
---
+++ src/pydmm/utils/upgrade/run_config.py
@@ -0,0 +1,5 @@
+
+# Due to dependencies, maybe this is probably best not to be here? Perhaps a cmdline util.
+
+# from prev_ver import YamlLoader as old_loader
+from ...run_config import YamlLoader as new_loader
