95ccfa11a496e5f709e16b7aba32b7680a4c785e
scott-diprose <scott.on.github@gmail.com>
2022-04-26 21:00:40 +1000

    clean up to push to main

--- src/data_object.py
+++
@@ -1,4 +0,0 @@
-# basic fields/columns, datatype, nullable
-# to be stored in data_schema folder
-
-# e.g. use case could be to create HSTG tables
--- src/utils/upgrade/data_connection.py
+++
@@ -1,5 +0,0 @@
-
-# Due to dependencies, maybe this is probably best not to be here? Perhaps a cmdline util.
-
-# from prev_ver import YamlLoader as old_loader
-from ...data_connection import YamlLoader as new_loader
