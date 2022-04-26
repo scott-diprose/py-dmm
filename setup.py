4fe6abe69245727bd98da8299234c1768e159425
scott-diprose <scott.on.github@gmail.com>
2022-04-26 19:45:22 +1000

    before name change

--- setup.py
+++ setup.py
@@ -1,22 +1,5 @@
-# -*- coding: utf-8 -*-
+# only present in order to support editable installs
+# (python -m setup.py develop / pipenv install -e)
+from setuptools import setup
 
-from setuptools import setup, find_packages
-
-
-with open('README.md') as f:
-    readme = f.read()
-
-with open('LICENSE') as f:
-    license = f.read()
-
-setup(
-    name='pydmm',
-    version='0.1.0',
-    description=
-    'Object model for standardising the structure of metadata applicable for mapping from one source data set to a target. Potentially being reshaped during the transfer.',
-    long_description=readme,
-    author='Scott Diprose',
-    author_email='scott.on.github@gmail.com',
-    url='https://github.com/scott-diprose/pydmm',
-    license=license,
-    packages=find_packages(exclude=('tests', 'docs')))
+setup()
--- src/pydmm/__init__.py
+++
@@ -1,4 +0,0 @@
-# # lift sub modules
-# from .utils import *
-
-__all__ = ['data_connection', 'mapping', 'run_config']
